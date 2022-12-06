179a4d30bd43952e6b09589b523c74a9c3a46edb
Prathmesh Bhondave <prathmesh.bhondave2@gmail.com>
2022-12-06 09:11:07 +0530

    (leetcode) BalancedBinaryTree added

---
+++ Binary Tree/BalancedBinaryTree.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int balanceChecker(TreeNode* root){
+        if(root == NULL){
+            return true;
+        }
+
+        int left = balanceChecker(root->left);
+        int right = balanceChecker(root->right);
+
+        if(abs(left-right) > 1 || left == -1 || right == -1){
+            return -1;
+        }
+
+        return max(left, right) + 1;
+    }
+
+    bool isBalanced(TreeNode* root) {
+       
+        if(balanceChecker(root) == -1){
+            return false;
+        }
+
+        return true;
+        
+    }
+};
