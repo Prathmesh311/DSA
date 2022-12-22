4451c0b22273d24bf6e516b8878f017f08b0c5fa
Prathmesh Bhondave <prathmesh.bhondave2@gmail.com>
2022-12-22 08:09:28 +0530

    (leetcode)ConvertSortedArraytoBinarySearchTree add

---
+++ BinarySearchTree/ConvertSortedArraytoBinarySearchTree.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* createBST(vector<int>& nums, int start, int end){
+        if(start > end){
+            return NULL;
+        }
+
+        int mid = (start+end)/2;
+
+        TreeNode* currRoot = new TreeNode(nums[mid]);
+
+
+        currRoot->left = createBST(nums, start, mid-1);
+        currRoot->right = createBST(nums, mid+1, end);
+        
+        return currRoot;
+    }
+
+
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        
+        return createBST(nums, 0, nums.size()-1);
+    }
+};
