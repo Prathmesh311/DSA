7affaff24682dd56c02ca01c5afe80c986258f5f
Prathmesh Bhondave <prathmesh.bhondave2@gmail.com>
2023-02-25 12:00:02 +0530

    (leetcode) Flood Fill Updated

--- Graph/FloodFill.cpp
+++ Graph/FloodFill.cpp
@@ -1,45 +1,33 @@
 class Solution {
 public:
-    int visited[51][51];
-    void DFSColorFill(vector<vector<int>>& image, int sr, int sc, int currColor, int color){
-        //Base condition
-        if(sr<0 || sc<0 || sr>=image.size() || sc>=image[0].size()){ //checking boundaries of matrix
+    int map[51][51] = {0};
+
+    void coloring(vector<vector<int>> & image, int sr, int sc, int currColor, int color){
+        if(sr >= image.size() || sc >= image[0].size() || sr < 0 || sc < 0){ //checking boundaries of matrix
             return;
         }
 
-        if(image[sr][sc] != currColor){     //if Currnode of diff color return
+        if(map[sr][sc] == 1){            //if node is visited previously then return
             return;
         }
 
-        if(visited[sr][sc] == 1){           //if node is visited previously then return
+        if(image[sr][sc] != currColor){  //if Currnode of diff color return
             return;
         }
 
-        visited[sr][sc] = 1;
-
+        map[sr][sc] = 1;
         image[sr][sc] = color;
+        
 
-        DFSColorFill(image, sr-1, sc, currColor, color);  //DFS over all 4 directions
-        DFSColorFill(image, sr, sc-1, currColor, color);
-        DFSColorFill(image, sr+1, sc, currColor, color);
-        DFSColorFill(image, sr, sc+1, currColor, color);
-
-        return;
-
+        coloring(image, sr+1, sc, currColor, color);    //DFS over all 4 directions
+        coloring(image, sr-1, sc, currColor, color);
+        coloring(image, sr, sc+1, currColor, color);
+        coloring(image, sr, sc-1, currColor, color);
     }
 
-
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        //int visited[51][51];
-        for(int i=0; i < 51; i++){
-            for(int j=0; j < 51; j++){
-                visited[i][j] = 0;
-            }
-        }
-        int currColor = image[sr][sc];
-        DFSColorFill(image, sr, sc, currColor, color);
+        coloring(image, sr, sc, image[sr][sc], color);
 
         return image;
-        
     }
 };
