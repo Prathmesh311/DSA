50360fd6f2d1ddc0b767b8bbb6223a077bde8e90
Prathmesh Bhondave <prathmesh.bhondave2@gmail.com>
2023-03-10 08:59:30 +0530

    (leetcode) KthSmallestElementInBST updated

--- BinarySearchTree/KthSmallestElementInBST.cpp
+++ BinarySearchTree/KthSmallestElementInBST.cpp
@@ -11,7 +11,7 @@
  */
 class Solution {
 public:
-    void helper(TreeNode* root, vector<int>& preOrder){
+    void helper(TreeNode* root, vector<int>& preOrder){   //storing method
         if(root==NULL){
             return;
         }
@@ -20,12 +20,28 @@ public:
         preOrder.push_back(root->val);
         helper(root->right, preOrder);
     }
+    
+    int ans = -1;
+    void helperRec(TreeNode* root, int& k){              //without storing nodes
+        if(root==NULL){
+            return;
+        }
+
+        helperRec(root->left, k);
+        k--;
+        if(k == 0){
+            ans = root->val;
+        }
+        helperRec(root->right, k);
+    }
 
 
     int kthSmallest(TreeNode* root, int k) {
         vector<int> preOrder;
         helper(root, preOrder);
-
         return preOrder[k-1];
+        
+        helper(root, k);
+        return ans;
     }
 };
