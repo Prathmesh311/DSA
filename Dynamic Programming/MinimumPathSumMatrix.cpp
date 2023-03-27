d6d2f6841736ca82c58fad6d7591a7f7a13f7cd4
Prathmesh Bhondave <prathmesh.bhondave2@gmail.com>
2023-03-27 08:29:26 +0530

    (leetcode) MinimumPathSumMatrix update

--- Dynamic Programming/MinimumPathSumMatrix.cpp
+++ Dynamic Programming/MinimumPathSumMatrix.cpp
@@ -1,6 +1,33 @@
 class Solution {
 public:
+    int minPath(vector<vector<int>>& grid, int dp[][201], int row, int col){
+        if(row < 0 || col < 0 || row >= grid.size() || col >= grid[0].size()){
+            return INT_MAX;
+        }
+
+        if(dp[row][col] != 0){
+            return dp[row][col];
+        }
+
+        int right = minPath(grid, dp, row, col+1);
+        int down = minPath(grid, dp, row+1, col);
+
+        if(right == INT_MAX && down == INT_MAX){
+            return grid[row][col];
+        }
+
+        dp[row][col] = min(right, down) + grid[row][col];
+
+        return dp[row][col];
+    }
+
+    
     int minPathSum(vector<vector<int>>& grid) {
+        //Recursive method
+        //int dp[201][201] = {0};
+        //return minPath(grid, dp, 0, 0);
+        
+        //Tabular Method
         int rows = grid.size();
         int cols = grid[0].size();
         
