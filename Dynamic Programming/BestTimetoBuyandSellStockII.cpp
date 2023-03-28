c3630d13dbf4872ca7fec83d9f8f8d16f36c71e0
Prathmesh Bhondave <prathmesh.bhondave2@gmail.com>
2023-03-28 09:17:42 +0530

    (leetcode) BestTimetoBuyandSellStockII updated

--- Dynamic Programming/BestTimetoBuyandSellStockII.cpp
+++ Dynamic Programming/BestTimetoBuyandSellStockII.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int maxProfit = 0;
+        int totalProfit= 0;
 
+        int buyPrice = prices[0];
         for(int i=1; i < prices.size(); i++){
-            if(prices[i] > prices[i-1]){
-                maxProfit += prices[i] - prices[i-1];
+            if(buyPrice < prices[i]){
+                totalProfit += prices[i] - buyPrice;
+                buyPrice = prices[i];
+            }
+            else{
+                buyPrice = prices[i];
             }
         }
 
-        return maxProfit;
+        return totalProfit;
     }
 };
