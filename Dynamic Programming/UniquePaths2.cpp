9264538c2adf01c6f54f8889ebc34aa7a5d5e6bd
Prathmesh Bhondave <prathmesh.bhondave2@gmail.com>
2023-04-06 10:36:04 +0530

    (leetcode) UniquePaths2 updated

--- Dynamic Programming/UniquePaths2.cpp
+++ Dynamic Programming/UniquePaths2.cpp
@@ -1,6 +1,35 @@
 class Solution {
 public:
+    int uniquePaths(vector<vector<int>>& grid, vector<vector<int>> &dp, int row, int col){
+        if(row < 0 || col < 0){
+            return 0;
+        }
+
+        if(grid[row][col] == 1){
+            return  dp[row][col] = 0;
+        }
+
+        if(row == 0 && col == 0){
+            return dp[row][col] = 1;
+        }
+
+        if(dp[row][col] != 0){
+            return dp[row][col];
+        }
+
+        int left = uniquePaths(grid, dp, row, col-1);
+        int top  = uniquePaths(grid, dp, row-1, col);
+
+        dp[row][col] = left + top;
+
+        return dp[row][col];
+    }
+    
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        //vector<vector<int>> dp(101, vector<int>(101, 0));
+        //return uniquePaths(obstacleGrid, dp, obstacleGrid.size()-1, obstacleGrid[0].size()-1);
+        
+        
         int dp[100][100] = {0};
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
@@ -39,9 +68,7 @@ public:
                     }
                 }
             }
-        }
-        
-        
+        } 
         
         return dp[m-1][n-1];
     }
