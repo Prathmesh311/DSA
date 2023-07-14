0d5a73aa49d013fbcb45326266ccd48432f66ca9
Prathmesh Bhondave <prathmesh.bhondave2@gmail.com>
2023-07-14 08:37:47 +0530

    (leetcode) TwoSum2 added

---
+++ Array/TwoSum2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        map<int, int> map;
+
+        for(int i=0; i < nums.size(); i++){
+            if(map.find(target - nums[i]) != map.end()){
+                ans.push_back(map[target-nums[i]]);
+                ans.push_back(i+1);
+                return ans;
+            }
+          
+            if(map.find(nums[i]) == map.end()){
+                map.insert(make_pair(nums[i], i+1));
+            }
+        }
+        
+        return ans;
+    }
+};
