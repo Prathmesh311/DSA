ff5baf2a257a434ca7c2c3ddc3aa1cca3b018a7e
Prathmesh Bhondave <prathmesh.bhondave2@gmail.com>
2023-07-17 08:51:03 +0530

    (leetcode) BitwiseANDofNumbersRange added

---
+++ BitManipulation/BitwiseANDofNumbersRange.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+      int counter = 0;
+
+      while(left != right){
+          left>>=1;
+          right>>=1;
+          counter++;
+      }
+
+      return left<<=counter;
+    }
+};
