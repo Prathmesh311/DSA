92bc528876cfd12d65b05e4d8f01610daee50076
Prathmesh Bhondave <prathmesh.bhondave2@gmail.com>
2023-07-28 10:16:19 +0530

    (leetcode) LongestIncresingSubsequence update

--- Dynamic Programming/LongestIncresingSubsequence.cpp
+++ Dynamic Programming/LongestIncresingSubsequence.cpp
@@ -19,8 +19,32 @@ public:
         return dp[curr][prev+1] = max(include, exclude);
     }
 
+     int solveLIS(vector<int>& nums){
+        vector<int> ans;
+        int n = nums.size();
+
+        if(n == 0){
+            return 0;
+        }
+
+        ans.push_back(nums[0]);
+        for(int i = 1; i < n; i++){
+            if(nums[i] > ans.back()){
+                ans.push_back(nums[i]);
+            }
+            else{
+                int index = lower_bound(ans.begin(), ans.end(), nums[i]) - ans.begin();
+                ans[index] = nums[i];
+            }
+           
+        }
+
+        return ans.size();
+    }
 
     int lengthOfLIS(vector<int>& nums) {
+
+        return solveLIS(nums);
         vector<vector<int>> dp(nums.size(), vector<int>(nums.size(), -1));
         return dfs(nums, 0, -1, dp);
     }
