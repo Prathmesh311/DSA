6c49f9d4b13948ff0e26107ec0e5478150a0b6e4
Prathmesh Bhondave <prathmesh.bhondave2@gmail.com>
2023-09-15 09:28:46 -0700

    (leetcode) Permutations updated

--- Backtracking/Permutations.cpp
+++ Backtracking/Permutations.cpp
@@ -1,29 +1,23 @@
-#include<iostream>
-using namespace std;
+class Solution {
+public:
+    void findPermutations(vector<int>& nums, vector<vector<int>> &ans, int index){
+        if(index >= nums.size()){
+            ans.push_back(nums);
+            return;
+        }
 
-void permutations(char *in, int i){
-    //Base case
-    if(in[i] == '\0'){
-        cout<<in<<" ";
-        return;
+        for(int j = index; j < nums.size(); j++){
+            swap(nums[index], nums[j]);
+            findPermutations(nums, ans, index+1);
+            swap(nums[index], nums[j]);
+        }
     }
 
-    //Recursive case
-    for(int j=i; in[j] != '\0'; j++){
-        swap(in[i], in[j]);
-        permutations(in, i+1);
 
-        //Backtracking
-        swap(in[i], in[j]);
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+
+        findPermutations(nums, ans, 0);
+        return ans;
     }
-}
-
-int main(){
-    char in[100];
-
-    cin>>in;
-
-    permutations(in, 0);
-   
-    return 0;
-}
+};
