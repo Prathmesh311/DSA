1f750150bf40a2623d07201247783eec1b6a44bd
Prathmesh Bhondave <prathmesh.bhondave2@gmail.com>
2023-10-23 13:13:10 -0700

    (leetcode) Candy added

---
+++ Greedy algorithms/Candy.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        vector<int> candies(ratings.size(), 1);
+
+        for(int i=1; i < ratings.size(); i++){
+                candies[i] = candies[i-1] + 1;
+            }
+        }
+
+        for(int i=ratings.size()-2; i >= 0; i--){
+            if(ratings[i+1] < ratings[i] && candies[i] < candies[i+1]+1){
+                candies[i] = candies[i+1] + 1;
+            }
+        }
+
+        return  accumulate(candies.begin(),candies.end(),0);
+    }
+};
