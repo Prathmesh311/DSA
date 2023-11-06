f6aba472be188189bac1a4aa43ff529fba9ddd27
Prathmesh Bhondave <prathmesh.bhondave2@gmail.com>
2023-11-06 11:36:29 -0800

    (leetcode) SeatReservationManager updated

--- Heap/SeatReservationManager.cpp
+++ Heap/SeatReservationManager.cpp
@@ -1,24 +1,20 @@
 class SeatManager {
 public:
     priority_queue<int, vector<int>, greater<int>> heap;
-    vector<int> seats;
 
     SeatManager(int n) {
         for(int i=1; i <= n; i++){
             heap.push(i);
-            seats.push_back(0);
         }
     }
     
     int reserve() {
         int availableSeat = heap.top();
         heap.pop();
-        seats[availableSeat-1] = 1;
         return availableSeat;
     }
     
     void unreserve(int seatNumber) {
-        seats[seatNumber-1] = 0;
         heap.push(seatNumber);
     }
 };
