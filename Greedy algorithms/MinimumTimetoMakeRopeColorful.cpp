8e7e3e9e88eab6dc646167fc3202e764d335d2aa
Prathmesh Bhondave <prathmesh.bhondave2@gmail.com>
2023-12-27 14:20:14 -0800

    (leetcode) MinimumTimetoMakeRopeColorful updated

--- Greedy algorithms/MinimumTimetoMakeRopeColorful.cpp
+++ Greedy algorithms/MinimumTimetoMakeRopeColorful.cpp
@@ -3,6 +3,7 @@ public:
     int minCost(string colors, vector<int>& neededTime) {
         int cost = 0;
 
+        //method1: Greedy
         for(int i=1; i < colors.size(); i++){
             if(colors[i] == colors[i-1]){
                 int j = i;
@@ -19,6 +20,23 @@ public:
                 cost += (totalCost - currMax);
 
                 i = j-1;
+            }
+        }
+
+
+        //Method2: 2-pointer - eleminating 1 balloon amount current 2 balloons
+        int left = 0;
+
+        for(int right = 1; right < colors.size(); right++){
+            if(colors[left] == colors[right]){
+                if(neededTime[left] < neededTime[right]){
+                    cost += neededTime[left];
+                    left = right;
+                }else{
+                    cost += neededTime[right];
+                }
+            }else{
+                left = right;
             }
         }
 
