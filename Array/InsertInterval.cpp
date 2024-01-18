84d1f2ef18df82d8171309e63be1db06cb69469d
Prathmesh Bhondave <prathmesh.bhondave2@gmail.com>
2024-01-18 14:27:51 -0800

    (leetcode) InsertInterval added

---
+++ Array/InsertInterval.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> ans;
+
+        for(int i=0; i < intervals.size(); i++){
+            if(newInterval[1] < intervals[i][0]){
+                ans.push_back(newInterval);
+                copy(intervals.begin() + i, intervals.end(), back_inserter(ans));
+                return ans;
+            }
+            else if(intervals[i][1] < newInterval[0]){
+                ans.push_back(intervals[i]);
+            }else{
+                newInterval[0] = min(newInterval[0], intervals[i][0]);
+                newInterval[1] = max(newInterval[1], intervals[i][1]);
+            }
+        }
+
+        ans.push_back(newInterval);
+
+        return ans;
+    }
+};
