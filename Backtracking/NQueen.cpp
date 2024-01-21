d5e12e60e1d22672e144219a2f892f1c6729fe38
Prathmesh Bhondave <prathmesh.bhondave2@gmail.com>
2024-01-21 11:58:15 -0800

    (leetcode) NQueen updated

--- Backtracking/NQueen.cpp
+++ Backtracking/NQueen.cpp
@@ -65,6 +65,55 @@ bool nQueen(int board[][10], int i, int n){
 
 }
 
+//Easy to implement method
+class Solution {
+public:
+    void NQueens(vector<vector<string>> board, vector<vector<string>> &comb, set<int> posDiag, set<int> negDiag,vector<int> &cols, int row){
+        if(row >= board.size()){
+            vector<string> v;
+            for(int i=0; i < board.size(); i++){
+                string str = "";
+                for(int j=0; j < board[0].size(); j++){
+                    str += board[i][j];
+                }
+                v.push_back(str);
+            }
+            comb.push_back(v);
+        }
+
+        for(int i=0; i < board[0].size(); i++){
+            if(cols[i] == 0 && posDiag.find(row+i) == posDiag.end() && negDiag.find(row-i) == negDiag.end()){
+
+                cols[i] = 1;
+                board[row][i] = "Q";
+                posDiag.insert(row+i);
+                negDiag.insert(row-i);
+
+                NQueens(board, comb, posDiag, negDiag, cols, row+1);
+                
+                cols[i] = 0;
+                board[row][i] = ".";
+                posDiag.erase(row+i);
+                negDiag.erase(row-i);
+            }
+        }
+    }
+
+
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> comb;
+        vector<vector<string>> board(n, vector<string>(n, "."));
+        vector<int> cols(n, 0);
+        set<int> posDiag;
+        set<int> negDiag;
+    
+        
+
+        NQueens(board, comb, posDiag, negDiag, cols, 0);
+        return comb;
+    }
+};
+
 int main() {
    int board[10][10] = {0};
 
