eb12ecfdc68c06d5838bbd2da6cac21408bb8a2a
Prathmesh Bhondave <prathmesh.bhondave2@gmail.com>
2024-01-21 14:22:14 -0800

    (leetcode) GasStation updated

--- Greedy algorithms/GasStation.cpp
+++ Greedy algorithms/GasStation.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int index = 0;
-        int totalRem = 0;
-
-        for(int i=0; i < gas.size(); i++){
-            totalRem += gas[i] - cost[i];
-        }
-
-        if(totalRem < 0){        //If total Of diff is less than 0 no path exist
-            return -1;
-        }
-
+        int position = 0;
+        int sum = 0;
         int total = 0;
-        for(int i=0; i < gas.size(); i++){  If you reach at the end of array without loosing gas. Return index of 1st starting position. 
-            total += gas[i] - cost[i];
+        for(int i=0; i < gas.size(); i++){
+            sum += gas[i] - cost[i];
 
-            if(total < 0){
-                total = 0;
-                index = i+1;
+            if(sum < 0){
+                total += sum;
+                sum = 0;
+                position = i+1;
             }
         }
+    
+        total += sum;
+        
+        if(total >= 0){
+            return position;
+        }
 
-        return index;
+        return -1;
     }
 };
