ac392e787db2fee2f371fc957438cb407e1d67a1
Prathmesh Bhondave <prathmesh.bhondave2@gmail.com>
2024-01-22 14:55:17 -0800

    (leetcode) TrappingRainWater updted

--- Array/TrappingRainWater.cpp
+++ Array/TrappingRainWater.cpp
@@ -1,29 +1,53 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        vector<int> maxLeft;
-        vector<int> maxRight;
-        
-        int maxLeftTillNow = 0;
-        for(int i=0; i < height.size(); i++){
-            maxLeftTillNow = max(maxLeftTillNow, height[i]);
-            maxLeft.push_back(maxLeftTillNow);
-        }
 
-        int maxRightTillNow = 0;
-        for(int i=height.size()-1; i >= 0; i--){
-            maxRightTillNow = max(maxRightTillNow, height[i]);
-            maxRight.insert(maxRight.begin() ,maxRightTillNow);
+    //Method 1: with storing leftMax & rightMax
+    int trap(vector<int>& height) {
+        vector<int> leftMax(height.size(), 0);
+        vector<int> rightMax(height.size(), 0);
+        
+        for(int i=1; i < height.size(); i++){
+            leftMax[i] = max(height[i-1], leftMax[i-1]);
+        }
+        
+        for(int i=height.size()-2; i >= 0; i--){
+            rightMax[i] = max(height[i+1], rightMax[i+1]);
         }
 
         int trappedWater = 0;
-        for(int i=0; i < height.size(); i++){
-            if(maxLeft[i] > height[i] && height[i] < maxRight[i]){
-                int rainAmount = min(maxLeft[i], maxRight[i]) - height[i];
-                trappedWater += rainAmount; 
+
+        for(int i = 1; i < height.size()-1; i++){
+            int maxHeight = min(rightMax[i], leftMax[i]);
+            trappedWater += max(0, maxHeight - height[i]);
+        }
+
+        return trappedWater;
+    }
+
+    //Method 2: without storing leftMax & rightMax
+    int trap(vector<int>& height) {
+        int n = height.size();
+        if (n <= 2) {
+            return 0; // No trapped water if there are less than 3 bars.
+        }
+
+        int leftMax = 0, rightMax = 0;
+        int left = 0, right = n - 1;
+        int trappedWater = 0;
+
+        while (left < right) {
+            if (height[left] <= height[right]) {
+                leftMax = max(leftMax, height[left]);
+                trappedWater += max(0, leftMax - height[left]);
+                left++;
+            } else {
+                rightMax = max(rightMax, height[right]);
+                trappedWater += max(0, rightMax - height[right]);
+                right--;
             }
         }
 
         return trappedWater;
     }
+
 };
