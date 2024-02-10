b8609c378bb3ef60246428dd0ebb10f3e22f9824
Prathmesh Bhondave <prathmesh.bhondave2@gmail.com>
2024-02-09 18:13:06 -0800

    (leetcode) IsGraphBipartite updated

--- Graph/IsGraphBipartite.cpp
+++ Graph/IsGraphBipartite.cpp
@@ -20,7 +20,7 @@ public:
         return true;
     }
 
-
+    //Method 1: DFS
     bool isBipartite(vector<vector<int>>& graph) {
         int visited[101] = {0};   //0 - not visited; 1 = visited and color 1; 2 - visited and color 2
 
@@ -37,4 +37,39 @@ public:
         
         return true;
     }
+
+
+   //Method 2: BFS
+    bool isBipartite(vector<vector<int>>& graph) {
+        queue<int> q;
+        vector<int> color(graph.size(), -1);
+        vector<int> visited(graph.size(), 0);
+
+
+        for(int i=0; i < graph.size(); i++){
+            if(visited[i] == 0){
+
+                q.push(i);
+                color[i] = 1;
+
+                while(!q.empty()){
+                    int node = q.front();
+                    q.pop();
+                    visited[node] = 1;
+
+                    for(int nextNode : graph[node]){
+                        if(visited[nextNode] == 0){
+                            q.push(nextNode);
+                            color[nextNode] = 1 - color[node];
+                        }else if(color[node] == color[nextNode]){
+                            return false;
+                        }
+                    }
+                }
+             }
+        }
+
+        return true;
+    }
+
 };
