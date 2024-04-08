4332ddbadd06d57383584e9b58d90149988dc731
Prathmesh Bhondave <prathmesh.bhondave2@gmail.com>
2024-04-08 14:17:24 -0700

    (leetcode) StringCompression updated

--- Trie and String/StringCompression.cpp
+++ Trie and String/StringCompression.cpp
@@ -1,49 +1,37 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        int totalLen = 0;
-        vector<char> compress;
+        int compressedLen = 0;
+        int i = 1;
+        int counter = 1;
 
+        for(; i < chars.size(); i++){
+            if(chars[i] == chars[i-1]){
+                counter++;
+            }
+            else{
+                chars[compressedLen++] = chars[i-1];
+                string currCount = to_string(counter);
 
-        for(int i=0; i < chars.size(); i++){   //Interating over array of string
-            int currLen = 0;
-            char currChar = chars[i];
-
-            for(int j = i; j < chars.size(); j++){
-                if(chars[j] == currChar){      // count consecutive occurances of specific char 
-                    currLen++;
+                if(counter > 1){
+                    for(char c : currCount){
+                        chars[compressedLen++] = c;
+                    }
                 }
-                else{
-                    break;
-                }
-            }
-          
-            compress.push_back(currChar);     //push char in new string
-            
-            if(currLen == 1){
-                continue;
-            }
-            
-            i = i + currLen-1;
-            stack<int> stack;
-            while(currLen > 0){               //splitting number of occurances in single char 
-                int rem = currLen % 10;
-                currLen  = currLen / 10;
 
-                stack.push(rem);
+                counter = 1;
             }
-
-            while(!stack.empty()){            //pushing occurances single chars to new string
-                int num = stack.top();
-                stack.pop();
-                char c = num + '0';
-                compress.push_back(c);
-            }
-
         }
-      
-        chars = compress;                     //assiging new string 
-        return chars.size();                  //returning new string size
-        
+
+        chars[compressedLen++] = chars[i-1];
+        string currCount = to_string(counter);
+
+        if(counter > 1){
+            for(char c : currCount){
+                chars[compressedLen++] = c;
+            }
+        }
+
+        return compressedLen;
     }
 };
