54205607731457e1eb6bf1af839719e3cb294e82
Prathmesh Bhondave <prathmesh.bhondave2@gmail.com>
2024-05-19 13:07:32 -0700

    (leetcode) DailyTemperatures updated

--- Stack/DailyTemperatures.cpp
+++ Stack/DailyTemperatures.cpp
@@ -1,55 +1,21 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        vector<int> tempSpan;
         stack<int> stack;
+        vector<int> waitDays(temperatures.size(), 0);
 
+        for(int i=0; i < temperatures.size(); i++){
 
-        for(int i=temperatures.size()-1; i >= 0; i--){
-            if(stack.size() == 0){
-                stack.push(i);
-                tempSpan.push_back(0);
-            }else{
-                while(stack.size() > 0 && temperatures[i] >= temperatures[stack.top()]){
-                    stack.pop();
-                }
-
-                if(stack.size() == 0){
-                    stack.push(i);
-                }
-
-                int span = stack.top() - i;
-                tempSpan.push_back(span);
-                stack.push(i);
-            }
-        }
-
-        reverse(tempSpan.begin(), tempSpan.end());
-        
-        return tempSpan;
-    }
-
-    //Easier method faster than above method
-    vector<int> dailyTemperatures(vector<int>& temperatures) {
-        int n = temperatures.size();
-        vector<int> ans(n, 0);
-        stack<int> stack;
-
-        stack.push(0);
-
-        for(int i=1; i < n; i++){
-           
-            while(!stack.empty() && temperatures[i] > temperatures[stack.top()]){
-                ans[stack.top()] = i - stack.top();
+            while(!stack.empty() && temperatures[stack.top()] < temperatures[i]){
+                int topIndex = stack.top();
                 stack.pop();
+
+                waitDays[topIndex] = i - topIndex;
             }
-
-
+            
             stack.push(i);
         }
 
-        return ans;
+        return waitDays;
     }
 };
-
-
