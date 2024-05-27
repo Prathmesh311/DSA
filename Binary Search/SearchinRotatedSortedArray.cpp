2bbf7c2b1c6840ebe48c20102a3136b18cfa6dbf
Prathmesh Bhondave <prathmesh.bhondave2@gmail.com>
2024-05-27 12:23:34 -0700

    SearchinRotatedSortedArray updated

--- Binary Search/SearchinRotatedSortedArray.cpp
+++ Binary Search/SearchinRotatedSortedArray.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int start = 0;
-        int end = nums.size()-1;
+        int left = 0;
+        int right = nums.size()-1;
 
-        while(start <= end){
-            int mid = (start + end)/2;
+        while(left <= right){
+            int mid = (left + right)/2;
 
             if(nums[mid] == target){
                 return mid;
             }
-            
-            if(nums[start] <= nums[mid]){
-                if(nums[mid] < target || nums[start] > target){
-                    start = mid + 1;
+
+            if(nums[left] <= nums[mid]){
+                if(nums[left] <= target && target <= nums[mid]){
+                    right = mid-1;
+                }else{
+                    left = mid+1;
                 }
-                else{
-                    end = mid - 1;
-                }
-            }
-            else{
-                if(nums[mid] > target || nums[end] < target){
-                    end = mid - 1;
-                }
-                else{
-                    start = mid + 1;
+            }else{
+                if(nums[mid] <= target && target <= nums[right]){
+                    left = mid+1;
+                }else{
+                    right = mid-1;
                 }
             }
+
         }
 
         return -1;
