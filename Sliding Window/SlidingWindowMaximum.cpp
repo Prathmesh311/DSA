ee3d9fd36411c872da62798ed81a7d81717cdc9d
Prathmesh Bhondave <prathmesh.bhondave2@gmail.com>
2024-06-29 18:28:32 -0700

    Update SlidingWindowMaximum

--- Sliding Window/SlidingWindowMaximum.cpp
+++ Sliding Window/SlidingWindowMaximum.cpp
@@ -1,38 +1,31 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        vector<int> ans;
-        deque<int> q;
+        vector<int> result;
+        deque<int> dq;
 
-        int start = 0;
-        int end = 0;
+        int left = 0;
+        int right = 0;
 
-        while(end-start < k){
-            while(!q.empty() && q.back() < nums[end]){
-                q.pop_back();
+        for(; right < nums.size(); right++){
+            while(!dq.empty() && nums[right] > dq.back()){
+                dq.pop_back();
             }
-            q.push_back(nums[end]);
-            end++;
+
+            dq.push_back(nums[right]);
+
+            while(right - left >= k){
+                if(dq.front() == nums[left]){
+                    dq.pop_front();
+                }
+                left++;
+            }
+
+            if(right-left == k-1){
+                result.push_back(dq.front());
+            }
         }
 
-        while(end < nums.size()){
-            ans.push_back(q.front());
-
-            if(nums[start] == q.front()){
-                q.pop_front();
-            }
-            start++;
-
-            while(!q.empty() && q.back() < nums[end]){
-                q.pop_back();
-            }
-
-            q.push_back(nums[end]);
-            end++;
-        }
-
-        ans.push_back(q.front());
-
-        return ans;
+        return result;
     }
 };
