2c46e251767eeb31ab61876cb5e0698d0c599455
Prathmesh Bhondave <prathmesh.bhondave2@gmail.com>
2024-07-01 14:03:47 -0700

    Update GetEqualSubstringsWithinBudget

--- Sliding Window/GetEqualSubstringsWithinBudget.cpp
+++ Sliding Window/GetEqualSubstringsWithinBudget.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     int equalSubstring(string s, string t, int maxCost) {
-        int maxSubStr = 0;
-
         int left = 0;
-        int right = 0;
+        int maxLen = 0;
 
-        while(right < s.size()){
-            int diff = abs((s[right] - 'a') - (t[right]-'a'));
+        for(int right = 0; right < s.size(); right++){
+            maxCost -= abs((s[right] - 'a') - (t[right] - 'a'));
 
-            if(maxCost >= diff){ 
-                maxCost -= diff;
-                right++;
-                maxSubStr = max(maxSubStr, right-left);
-            }else{
-                maxCost += abs((s[left] - 'a') - (t[left]-'a'));
+            if(maxCost < 0){
+                maxCost += abs((s[left] - 'a') - (t[left] - 'a'));
                 left++;
             }
+
+            maxLen = max(maxLen, right - left + 1);
         }
 
-        return maxSubStr;
+        return maxLen;
     }
 };
