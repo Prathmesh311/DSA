580eed91db46cd95c823e295262e6e4645692167
Prathmesh Bhondave <prathmesh.bhondave2@gmail.com>
2025-03-08 00:52:31 -0800

    Update CoinChange

--- Dynamic Programming/CoinChange.cpp
+++ Dynamic Programming/CoinChange.cpp
@@ -1,73 +1,42 @@
-#include <iostream>
-using namespace std;
-
-// Top To Bottom Approach
-int coinChange(int n, int size, int coins[], int dp[])
-{
-    // Base case
-    if (n == 0)
-    {
-        return 0;
-    }
-
-    // Recursive case
-    //Checking data
-    if(dp[n] != 0){
-        return dp[n];
-    }
-
-    int minCost = INT8_MAX;
-    for (int i = 0; i < size; i++)
-    {
-        if (n - coins[i] >= 0)
-        {
-            int currCost = coinChange(n - coins[i], size, coins, dp) + 1;
-
-            if (currCost < minCost)
-            {
-                minCost = currCost;
-            }
+class Solution {
+public:
+    int findChange(vector<int> &coins, vector<vector<int>> &dp, int amount, int index){
+        if(amount == 0){
+            return 0;
         }
+
+        if(index >= coins.size() || amount < 0){
+            return 100001;
+        }
+
+        if(dp[index][amount] != INT_MAX){
+            return dp[index][amount];
+        }
+
+        int minChange = 100001;
+        for(int i=index; i < coins.size(); i++){
+            minChange = min(minChange, findChange(coins, dp, amount - coins[i], i) + 1);
+        }
+
+        return dp[index][amount] = minChange;
     }
 
-    //Memoization
-    dp[n] = minCost;
-    return minCost;
-}
+    int coinChange(vector<int>& coins, int amount) {
+        // vector<vector<int>> dp(coins.size()+1, vector<int>(amount+1, INT_MAX));
+        // int result = findChange(coins, dp, amount, 0); 
+        // return result >= 10001 ? -1 : result;
 
-// Bottom To Top Approach
-int coinChangeBT(int n, int size, int coins[]){
-    int dp[n] = {INT8_MAX};
-    
+        vector<int> dp(amount+1, 100001);
+        dp[0] = 0;
 
-    dp[0] = 0;
-    for(int j=1; j <= n; j++){
-        int minCost = INT8_MAX;
-        for(int i=0; i < size; i++){
-            if(j - coins[i] >= 0){
-                int currCost = dp[j-coins[i]] + 1;
-
-                if(currCost < minCost){
-                    minCost =  currCost;
+        for(int i = 1; i <= amount; i++){
+            for(int coin : coins){
+                if(i - coin >= 0){
+                    dp[i] = min(dp[i], dp[i-coin] + 1);
                 }
             }
         }
-        dp[j] = minCost;
+
+        return dp[amount] >= 10001 ? -1 : dp[amount];
     }
-
-    return dp[n];
-}
-
-int main()
-{
-    int n;
-    int coins[] = {1, 3, 5};
-    int dp[100] = {0};
-
-    cin >> n;
-    int size = sizeof(coins) / sizeof(int);
-
-    cout << coinChange(n, size, coins, dp) << endl;
-    cout<<coinChangeBT(n, size, coins);
-    return 0;
-}
+};
